6bc463396241a86fb426e5f3827b7bbf07211150
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-05-23 15:15:05 -0700

    Built the about page and routed to it

--- restaurant/views.py
+++ restaurant/views.py
@@ -4,3 +4,7 @@ from django.views import generic
 
 class HomePage(generic.TemplateView):
     template_name = "index.html"
+
+
+class AboutPage(generic.TemplateView):
+    template_name = "about.html"
