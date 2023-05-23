f7734d721a76892e641f8017398f84d3de2ee275
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-05-23 16:09:51 -0700

    Added a menu page

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
 
+from .models import Item
+
 
 class HomePage(generic.TemplateView):
     template_name = "index.html"
@@ -8,3 +10,8 @@ class HomePage(generic.TemplateView):
 
 class AboutPage(generic.TemplateView):
     template_name = "about.html"
+
+
+class MenuList(generic.ListView):
+    queryset = Item.objects.order_by("-date_added")
+    template_name = "menu.html"
