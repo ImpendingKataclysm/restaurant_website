37b4d39334536cc24709b269fc281b867099670f
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-05-25 16:37:49 -0700

    Created a database model for restaurant reservations

---
+++ restaurant/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+from .models import Reservation
+
+
+class ReservationForm(forms.Form):
+    location = forms.CharField()
+    party_name = forms.CharField()
+    party_size = forms.IntegerField()
+    date_reserved = forms.DateField()
+    time_reserved = forms.TimeField()
--- restaurant/models.py
+++ restaurant/models.py
@@ -43,7 +43,7 @@ class Location(models.Model):
 
 class Reservation(models.Model):
     party_name = models.CharField(max_length=1000)
-    party_size = models.IntegerField(max_length=50)
+    party_size = models.IntegerField()
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
     date_reserved = models.DateField()
     time_reserved = models.TimeField()
