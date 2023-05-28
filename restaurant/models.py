8dee307d3ce06b251cbc097f4fad6f49f4fc294b
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-05-28 16:32:05 -0700

    Added image url field to menu item table in database and displayed the image for each item on its details page

--- restaurant/models.py
+++ restaurant/models.py
@@ -24,6 +24,7 @@ class Item(models.Model):
     status = models.IntegerField(choices=STATUS, default=1)
     date_added = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
+    image_url = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return self.name
