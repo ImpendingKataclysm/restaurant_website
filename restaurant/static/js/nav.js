3a724b5bfcbbdc255b91b9916faccd5d7babcdc0
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-05-31 11:41:13 -0700

    Hide the nav bar by default on mobile and implement a button that toggles the nav visibility

---
+++ restaurant/static/js/nav.js
@@ -0,0 +1,9 @@
+nav = document.getElementById("site_nav")
+nav_btn = document.getElementById("nav_btn")
+
+nav_btn.addEventListener("click", showNav)
+
+function showNav()
+{
+    nav.classList.toggle("show");
+}
